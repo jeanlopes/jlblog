5675611cc6759c134850a4b190046d031713007a
Jean Lopes <jeanoliveiralopes@yahoo.com.br>
2015-08-18 18:39:08 -0300

    Update context.py

--- domain/data/context.py
+++ domain/data/context.py
@@ -9,7 +9,7 @@ from asq.initiators import query
 
 
 class Context(object):
-        #producao server = Server('https://jeanlopes:fp87694fbr@jeanlopes.cloudant.com')
+        #producao server = Server('https://jeanlopes:my_secret_pass@jeanlopes.cloudant.com')
         def __init__(self):
             self.server = Server('http://192.168.56.101:5984')
             self.db = self.server.get_db('jlblog')
